db35c314f5c9a6333e9ab1a24103ba79dfd910a7
Mouad Drif <mouaddrif18@gmail.com>
2023-09-08 12:37:21 +0100

    images added

--- lib/screens/focus_square.dart
+++ lib/screens/focus_square.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
 
 class FocusSquare extends StatelessWidget {
-  const FocusSquare({super.key, required this.muscleName});
+  const FocusSquare(
+      {super.key, required this.muscleName, required this.imagePath});
   final String muscleName;
-
+  final String imagePath;
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 30, vertical: 14),
-      decoration: BoxDecoration(
-          color: Colors.black, borderRadius: BorderRadius.circular(15)),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+            offset: Offset(8, 10),
+            blurRadius: 40,
+            color: Color.fromARGB(255, 208, 224, 237)),
+
+      ], color: Colors.white, borderRadius: BorderRadius.circular(15)),
       height: 160,
       width: 150,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           //image
-          Container(
+          Image.asset(
+            imagePath,
             height: 60,
-            width: 60,
-            color: Colors.amberAccent,
           ),
           Text(
             muscleName,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.fromARGB(255, 239, 232, 232),
       body: Container(
         padding: EdgeInsets.only(top: 70, left: 30, right: 30),
         child: Column(
@@ -50,6 +50,7 @@ class HomeScreen extends StatelessWidget {
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // ignore: prefer_const_literals_to_create_immutables
               children: [
                 Text(
                   "Your program",
@@ -73,24 +74,26 @@ class HomeScreen extends StatelessWidget {
             SizedBox(
               height: 20,
             ),
+
+            //traning card
             Container(
               height: 180,
               decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(offset: Offset(10,10),
-                  blurRadius:80,
-                  color: Color.fromARGB(255, 26, 35, 39).withOpacity(0.7)
-                  ),
-                  
-                ],
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(10, 10),
+                        blurRadius: 80,
+                        color:
+                            Color.fromARGB(255, 26, 35, 39).withOpacity(0.7)),
+                  ],
                   gradient: LinearGradient(
-                    colors:[
+                    colors: [
                       Color.fromARGB(255, 2, 17, 30).withOpacity(0.9),
-                    const Color.fromARGB(255, 184, 219, 234)],
+                      const Color.fromARGB(255, 184, 219, 234)
+                    ],
                     begin: Alignment.bottomLeft,
                     end: Alignment.topRight,
-
-                    ),
+                  ),
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(12),
                       topRight: Radius.circular(80),
@@ -98,31 +101,32 @@ class HomeScreen extends StatelessWidget {
                       bottomRight: Radius.circular(12))),
               padding: EdgeInsets.all(15),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                      Text(
-                      "New workout",
-                      style: TextStyle(color: Colors.white, fontSize: 15),
+                        Text(
+                          "New workout",
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        Text(
+                          "Legs Toning",
+                          style: TextStyle(color: Colors.white, fontSize: 24),
+                        ),
+                        Text(
+                          "and Glutes Workout",
+                          style: TextStyle(color: Colors.white, fontSize: 24),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Text(
-                      "Legs Toning",
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                    Text(
-                      "and Glutes Workout",
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    ],),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       crossAxisAlignment: CrossAxisAlignment.end,
@@ -130,25 +134,34 @@ class HomeScreen extends StatelessWidget {
                         Row(
                           children: [
                             // Icon(Icon.time),
-                            Icon(Icons.timer,color: Colors.white,size: 20,),
-                            SizedBox(width: 2,),
-                            Text("60 min",
-                            style: TextStyle(
-                              color: Colors.white),),
-                              
+                            Icon(
+                              Icons.timer,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            SizedBox(
+                              width: 2,
+                            ),
+                            Text(
+                              "60 min",
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ],
-                          
                         ),
                         Container(
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(60),
-                            boxShadow: [
-                              BoxShadow()
-                            ]
+                              borderRadius: BorderRadius.circular(50),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Color.fromARGB(255, 2, 17, 30),
+                                    offset: Offset(4, 8),
+                                    blurRadius: 10)
+                              ]),
+                          child: Icon(
+                            Icons.play_circle_fill,
+                            color: Colors.white,
+                            size: 50,
                           ),
-                          child: Icon(Icons.play_circle_fill,
-                          color: Colors.white,
-                          size: 50,),
                         )
                         // Icon(Icons.)
                       ],
@@ -161,46 +174,104 @@ class HomeScreen extends StatelessWidget {
 
             Container(
               decoration: BoxDecoration(
+                // ignore: prefer_const_literals_to_create_immutables
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(8, 10),
+                      blurRadius: 40,
+                      color: Color.fromARGB(255, 208, 224, 237)),
+                  BoxShadow(
+                      offset: Offset(-1, -5),
+                      blurRadius: 10,
+                      color: Color.fromARGB(255, 208, 224, 237))
+                ],
                 borderRadius: BorderRadius.circular(15),
-                color: Colors.blue,
+                color: Colors.white,
               ),
-              child: Row(children: [
-                Container(
-                  color: Colors.black,
-                  height: 50,
-                  width: 50,
-                ),
-                SizedBox(width: 25,),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("You are doing great",
-                      style: TextStyle(fontSize:18,color: Colors.blue[300]),),
-                      SizedBox(height: 8,),
-                      Text("keep it up"),
-                      Text("stick to your plan")
-                    ],
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  top: -20,
+                  child: Container(
+                    // color: Colors.black,
+                    child: Image.asset(
+                      "images/dumbbell.png",
+                      width: 90,
+                      height: 140,
+                    ),
                   ),
-                )
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 14),
+                  child: Image.asset(
+                    "images/runner.png",
+                    height: 90,
+                    width: 120,
+                  ),
+                ),
+                Row(children: [
+                  // Container(
+                  //   color: Colors.black,
+                  //   height: 50,
+                  //   width: 50,
+                  // ),
+                  SizedBox(
+                    width: 120,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "You are doing great",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.blue[500]),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "keep it up",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        Text(
+                          "stick to your plan",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  )
+                ]),
               ]),
             ),
-            SizedBox(height: 20,),
-            Row(children: [Text("Area of focus",
-            style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),)]),
+            SizedBox(
+              height: 20,
+            ),
+            // ignore: prefer_const_literals_to_create_immutables
+            Row(children: [
+              Text(
+                "Area of focus",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              )
+            ]),
 
-            SizedBox(height: 14,),
+            SizedBox(
+              height: 14,
+            ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FocusSquare(muscleName: 'Glues',),
-                FocusSquare(muscleName: 'Abs'),
-
-              ]
-            )
-
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              FocusSquare(
+                muscleName: 'Glues',
+                imagePath: 'images/abs.png',
+              ),
+              FocusSquare(
+                muscleName: 'Biceps',
+                imagePath: 'images/biceps.png',
+              ),
+            ])
           ],
         ),
       ),
